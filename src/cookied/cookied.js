e46a239bc63fe71797c9c79a141ed7c6944a4711
dalanao <dal437@nyu.edu>
2017-10-29 21:21:20 -0400

    Modified cookied file; created global object to store data of the sessions

--- src/cookied/cookied.js
+++ src/cookied/cookied.js
@@ -1,3 +1,35 @@
-const uuid = require('node-uuid');
+const uuid = require('uuid/v1');
 
 // manageSession and parseCookies
+const inMemorySessionDb = {};
+
+//parseCookies
+exports.parseCookies = function(req, res, next) {
+  const c = req.headers['cookie'];
+  req.hwCookies = {};
+  if (c){
+    c.split('; ').forEach(x => {
+      const y = x.split('=');
+      req.hwCookies[y[0]] = y[1];
+    });
+  };
+  console.log('cookie', req.hwCookies);
+	next();
+};
+
+//manageSession
+exports.manageSession = function(req, res, next) {
+  const sessionId = req.hwCookies['sessionId'];
+  if (!sessionId || !inMemorySessionDb[sessionId]){
+    const s = uuid();
+    console.log("session generated: " + s);
+    inMemorySessionDb[s] = {sessionId: s};
+    res.set('Set-Cookie', "sessionId=" + s + "; HttpOnly");
+    req.hwSession = inMemorySessionDb[s];
+  }
+  else if (inMemorySessionDb[sessionId]){
+    console.log("session already exists: " + sessionId);
+    req.hwSession = inMemorySessionDb[sessionId];
+  }
+  next();
+};
