1ecab79286d7a5ad0032100b55d832c273cc3e22
dalanao <dal437@nyu.edu>
2017-10-29 23:27:46 -0400

    Working on movies database

--- src/movies/db.js
+++ src/movies/db.js
@@ -1,1 +1,19 @@
 // db.js
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+const MONGO_URI = 'mongodb://localhost/hw05';
+
+//my schema goes here!
+const MovieSchema = new Schema({
+  title: String,
+  year: Number,
+  director: String,
+});
+
+const Movie = mongoose.model('Movie', Movie Schema);
+
+mongoose.connect(MONGO_URI);
+
+module.exports = {
+  Movie,
+};
